0e9f362dc7d9fca712ca9d993e34775a67119032
Ruben <A01630323@itesm.mx>
2015-03-13 21:40:26 -0600

    Mastery

---
+++ Mastery01.py
@@ -0,0 +1,5 @@
+print ("*******************")
+print ("*** HOLA MUNDO ***")
+print ("******************")
+print ("Hola Mundo")
+input ()
---
+++ Mastery20.py
@@ -0,0 +1,7 @@
+print ("*********************")
+print ("*** CONTAR LETRAS ***")
+print ("*********************")
+Palabras=['Si','Dos','Hola','Lapiz','Cereza','Celular']
+for Palabras in Palabras:
+    print("La palabra",Palabras,"tiene",len(Palabras),"letras.")        #len() permite contar los caracteres de una cadena
+input()
