59e403fad9ad3478e2dd07434db36e84254c50e1
Ruben <rurris-9@hotmail.com>
2015-04-06 19:27:04 -0500

    Mastery

---
+++ Mastery25.py
@@ -0,0 +1,23 @@
+print ("**************")
+print ("*** RANGOS ***")
+print ("**************")
+#
+for a in range (20):									#Imprimir los numeros del 0 al 19
+	print (a,end=',')
+#
+print ()
+#
+for b in range (5,40,2):								#Imprimir los numeros desde el 5 hasta el 40 de dos en dos
+	print (b,end=',')
+#
+print ()
+#
+for c in range (-8,-100,-5):							#Imprimir los numeros desde el -8 hasta el -100 de menos cinco en menos cinco
+	print (c,end=',')
+#
+print ()
+#
+print (list(range(10)))									#Imprimir una lista con valores desde el cero al nueve
+print (list(range(21,50,3)))							#Imprimir una lista desde el 21 hasta el 50 de tres en tres
+#
+input()
