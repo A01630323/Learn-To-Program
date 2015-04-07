e189f62394cf542afdfc7d66af606471d4cebc87
Ruben <rurris-9@hotmail.com>
2015-04-06 19:27:28 -0500

    Mastery

---
+++ Mastery26.py
@@ -0,0 +1,30 @@
+print ("***************")
+print ("*** CADENAS ***")
+print ("***************")
+#
+Nombre='Ruben Barajas Curiel'
+Carrera="Ingeniero en Tecnologias Electronicas"
+Universidad='''Instituto Tecnologico y de Estudios Superiores de Monterrey'''
+print (Nombre)
+print (Carrera)
+print (Universidad)
+print ()
+#
+A=123
+B=True
+C=4.567
+D=4+3j
+print ("La variable A es:",type(A))
+print ("La variable B es:",type(B))
+print ("La variable C es:",type(C))
+print ("La variable D es:",type(D))
+#
+Cadena1=str(A)																	#Transformar variable tipo "int" en cadena
+Cadena2=str(B)																	#Transformar variable tipo "boolean" en cadena
+Cadena3=str(C)																	#Transformar variable tipo "float" en cadena
+Cadena4=str(D)																	#Transformar variable tipo "complex" en cadena
+print ("Ahora la variable A es",type(Cadena1))
+print ("Ahora la variable B es",type(Cadena2))
+print ("Ahora la variable C es",type(Cadena3))
+print ("Ahora la variable D es",type(Cadena4))
+input ()
