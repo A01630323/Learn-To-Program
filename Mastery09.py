d3811753cfc2122ccddebdc8a21235975b4b1a22
Ruben <rurris-9@hotmail.com>
2015-04-07 22:33:39 -0500

    Mastery

---
+++ Mastery09.py
@@ -0,0 +1,17 @@
+print ("**********************")
+print ("*** TIPOS DE DATOS ***")
+print ("**********************")
+#En Python3 existen datos de tipo int, float, string, bool y complex
+a=1
+b=2.2
+c="Tres"
+d=True
+e=2j
+f="F"
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+print (type(e))
+print (type(f))
+input ()
