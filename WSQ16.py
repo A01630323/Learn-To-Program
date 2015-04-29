e16462d09113278f8138d66cc1e85b221873485d
Ruben <rurris-9@hotmail.com>
2015-04-29 17:10:07 -0500

    WSQ

--- WSQ16.py
+++ WSQ16.py
@@ -14,9 +14,9 @@ def Carros():
 			GasCiudad=GasCiudad+float(Linea[52:54])
 			GasCarretera=GasCarretera+float(Linea[55:57])
 		Contador=Contador+1
-	print("El promedio de gas usado en la ciudad es " ,GasCiudad/(len(Linea)/2))
-	print("El promedio de gas usado en la carretera es: " ,GasCarretera/(len(Linea)/2))
-	print("El precio promedio de un auto de gama media es: " ,Precio/(len(Linea)/2))
+	print("El promedio de gas usado en la ciudad es " ,(GasCiudad/(len(Linea)))/2)
+	print("El promedio de gas usado en la carretera es: " ,(GasCarretera/(len(Linea)))/2)
+	print("El precio promedio de un auto de gama media es: " ,(Precio/(len(Linea)))/2)
 #
 Titulo()
 Carros()
